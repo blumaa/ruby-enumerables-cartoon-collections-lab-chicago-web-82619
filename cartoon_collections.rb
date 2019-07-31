18b859b383a783a608d71600398df851d6ce29a8
Aaron Blum <blumaa@gmail.com>
2019-07-31 05:42:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,13 +14,11 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_long)
-  calls_long.each_with_index { |i, index|
-    if i.size > 4
-      # binding.pry
+  calls_long.each { |i|
+    if i.size > 10
       return true
-    else
-      return false
-    end   }
+    end
+    }
 end
 
 def find_the_cheese# code an argument here
