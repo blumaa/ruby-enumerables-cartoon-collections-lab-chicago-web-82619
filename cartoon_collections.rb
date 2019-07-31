3b3d66e98b3d007017bc32131353773120dc6d55
Aaron Blum <blumaa@gmail.com>
2019-07-31 04:37:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,8 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect { |x| x.capitalize }
-  binding.pry
+  # binding.pry
+  
 end
 
 def long_planeteer_calls# code an argument here
