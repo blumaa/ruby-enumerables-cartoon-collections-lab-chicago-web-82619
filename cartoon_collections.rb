c7a117324a6c311fdd454895d6a51ca2e71fd6ed
Aaron Blum <blumaa@gmail.com>
2019-07-31 06:02:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,6 +25,6 @@ end
 def find_the_cheese(ingredients)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  ingredients.each { 
-  ingredients.include?(cheese_types) { |thing| 
+  ingredients.each { |food| 
+    food = ""
 end
